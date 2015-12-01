19c558e5a54c3385747ebf28145fabc3903a4d10
nate-d-olson <nolson@nist.gov>
2015-12-01 07:02:29 -0500

    refseq full genomes and 16S extraction

---
+++ refseq_genomes/extract_16S.sh
@@ -0,0 +1,16 @@
+extract_16S(){
+    prefix=$(echo $fa | sed 's/.fna//')
+    if [ ! -e "$prefix.fasta" ]
+    then
+        echo $prefix
+        rnammer -S bacterial -m ssu -xml $prefix-16S.xml -gff $prefix-16S.gff -f $prefix-16S.fasta < $fa
+    fi
+}
+
+N=8
+(
+for fa in */*fna; do 
+   ((i=i%N)); ((i++==0)) && wait
+   extract_16S "$fa" & 
+done
+)
